910b974ce8e184933230ac9f834a52946fa6b81f
Slowhand0309 <slowhand0309@gmail.com>
2016-06-12 02:10:32 +0900

    Updated mrfile header

--- include/mrfile.h
+++ include/mrfile.h
@@ -4,8 +4,43 @@
 #ifndef MRFILE_H_
 #define MRFILE_H_
 
-namespace mr-io {
+#include "mrstream.h"
 
-} // namespace mr-io
+namespace mrio {
+
+class MrFile : public MrStream {
+public:
+  // Constructor.
+  MrFile();
+
+  // Destructor.
+  virtual ~MrFile();
+
+  // Open file.
+  virtual int open(const char *name);
+
+  // Close file.
+  virtual void close();
+
+  // Write file.
+  virtual int write(const void *data, unsigned int size);
+
+  // Read file.
+  virtual int read(void *data, unsigned int size);
+
+  // Write file to text.
+  int write(const char* text, ...);
+
+  // Get file size.
+  unsigned int getFileSize();
+
+  // Check file eof?.
+  bool isEof();
+
+private:
+  FILE *fp_;
+};
+
+} // namespace mrio
 
 #endif // MRFILE_H_
