42e03a9a48b2f110e922c14063cd3e1e8aab3f44
Slowhand0309 <slowhand0309@gmail.com>
2016-06-17 23:23:31 +0900

    Updated mrfile

--- lib/mrfile.cpp
+++ lib/mrfile.cpp
@@ -33,4 +33,24 @@ void MrFile::close() {
   open_ = false;
 }
 
+// Get file size.
+unsigned int MrFile::getFileSize() {
+  unsigned int pos = 0;
+  if (!isOpen()) {
+    return pos;
+  }
+
+  unsigned int current = ftell(fp_);
+  fseek(fp_, 0, SEEK_END);
+  pos = ftell(fp_);
+  fseek(fp_, current, SEEK_SET);
+
+  return pos;
+}
+
+// Check EOF.
+bool MrFile::isEof() {
+  return feof(fp_) == 0 ? false : true;
+}
+
 } // namespace mrio
